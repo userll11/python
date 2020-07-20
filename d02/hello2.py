36998153c752e0e7d66fee363654db8d26a43e2b
User <chenenlin2@gmail.com>
2020-07-20 20:54:06 +0800

    練習

---
+++ d02/hello2.py
@@ -0,0 +1,26 @@
+# 八進位
+a = 0o15
+print(a)
+
+# 十進位
+b = 15
+print(b)
+
+# 十六進位
+c = 0xab
+print(c)
+#顯示值
+print(float(3/2))
+print(float(3/3))
+print(float(5e-3))
+print(float(5e+3))
+
+#只顯示整數部分
+print(int(3/3))
+print(int(3/2))
+#顯示複數
+print(complex(5+4j))
+
+
+
+
---
+++ d02/hello7.py
@@ -0,0 +1,28 @@
+# 跳脫字元 \
+# 我想要印出單引號 ' 或雙引號 "
+print('\'')
+print("'")
+print("\"")
+print('"')
+print('\\')
+print('A\nB')#斷行
+print('A\tB')#空一格
+print('A\rB')#回車
+
+
+
+a=10
+b=20
+c=30
+# 續行符號
+d=a*b/c*b-\
+    a*b/b+a+b+c+b
+
+
+
+
+
+
+
+
+
